8ca514389ab7300e71a7738c1f1e8b790b225fd2
rohit singh <rohitsingh986491@gmail.com>
2024-10-24 23:50:49 +0530

    commited all changes till sign in and login

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ const { listingSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const { reviewSchema } = require("./schema.js");
 
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+//flash- use to display certain message and this message is stroed in temporary memory
+
 //public folder
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -39,120 +52,56 @@ async function main() {
 }
 
 
-const validateListing = (req, res, next)=>{
-  let { error } = listingSchema.validate(req.body);
-    // console.log(result);
-    if(error){
-      let errMsg = error.details.map((er) => er.message).join(",");
-      throw new ExpressError(400,errMsg)
-    }else{
-      next();
-    }
-}
-
-const validateReview = (req, res, next)=>{
-  let { error } = reviewSchema.validate(req.body);
-    // console.log(result);
-    if(error){
-      let errMsg = error.details.map((er) => er.message).join(",");
-      throw new ExpressError(400,errMsg)
-      
-    }else{
-      next();
-    }
-}
-
-//all listings route
-app.get("/listings", wrapAsync(async (req, res) => {
-  let allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
-
-// new route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-// show route
-app.get("/listings/:id", wrapAsync( async (req, res) => {
-  let { id } = req.params;
-  let details = await Listing.findById(id).populate("reviews");
-  res.render("listings/show.ejs", { details });
-}));
-
-//create route
-app.post(
-  "/listings",validateListing,
-  wrapAsync(async (req, res, next) => {
-    // let listing = req.body.listing;
-    // console.log(listing);
-    let newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  })
-);
-
-//edit route
-app.get("/listings/:id/edit",wrapAsync( async (req, res) => {
-  let { id } = req.params;
-  let info = await Listing.findById(id);
-  // console.log(info);
-  res.render("listings/update.ejs", { info });
-}));
-
-//update route
-app.put("/listings/:id",validateListing,wrapAsync( async (req, res) => {
-  if(!req.body.listing){
-    throw new ExpressError(400,"Send valid data for listing")
-  }
-  let { id } = req.params;
-  let listing = req.body.listing;
-  let updateVal = await Listing.findByIdAndUpdate(id, { ...listing });
-  res.redirect("/listings");
-}));
-
-//delete route
-app.delete("/listings/:id", wrapAsync( async (req, res) => {
-  let { id } = req.params;
-  let destory = await Listing.findByIdAndDelete(id);
-  console.log(destory);
-  res.redirect("/listings");
-}));
-
-//reviews route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
-  let { id } = req.params;
-  //finding the listing by the id
-  let listing = await Listing.findById(id);
-  //extracting the values of the reviews from the form
-  let newReview = new Review(req.body.review);
-  //pushing the review in the listings
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-  res.redirect(`/listings/${listing._id}`)
-}));
-
-//delete reviews
-app.delete("/listings/:id/reviews/:reviewId",async(req,res)=>{
-  let { id, reviewId } = req.params;
-  //to delete the reviews from the listing we have to use $pull 
-  await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}})
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-
-  // let { id } = req.params;
-  // let listing = req.params.id;
-  // console.log(listing);
-  // await Review.findById(req.params.id);
-  // res.redirect("/listings/<%listing._id%>");
-  
-})
+//session- used to save user credentials in the webssite
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 + 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 // main route
 app.get("/", (req, res) => {
   res.send("App is in the main route");
 });
+
+//middle to to define sessions
+app.use(session(sessionOptions));
+//middleware for flash
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//accessing the message that I have defined in the listing.js
+//Defining middleware to use the flash
+app.use((req,res,next)=>{
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// app.get("/demouser", async(req,res) =>{
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "rohit",
+//   });
+  
+//   let registered = await User.register(fakeUser, "helloworld");
+//   res.send(registered);
+// })
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*",(req,res,next)=>{
   next(new ExpressError(404,"Page not found!"))
