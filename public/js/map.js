189de29f3d78016da28f809ee5bc482d0138b2e9
rohit singh <rohitsingh986491@gmail.com>
2024-10-30 23:12:53 +0530

    Resolved merge conflicts

--- public/js/map.js
+++ public/js/map.js
@@ -17,43 +17,43 @@
         .addTo(map);
 
 
-    map.on('load', () => {
-          // Load an image from an external URL.
-          map.loadImage(
-              'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
-              (error, image) => {
-                  if (error) throw error;
+    // map.on('load', () => {
+    //       // Load an image from an external URL.
+    //       map.loadImage(
+    //           'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
+    //           (error, image) => {
+    //               if (error) throw error;
   
-                  // Add the image to the map style.
-                  map.addImage('cat', image);
+    //               // Add the image to the map style.
+    //               map.addImage('cat', image);
   
-                  // Add a data source containing one point feature.
-                  map.addSource('point', {
-                      'type': 'geojson',
-                      'data': {
-                          'type': 'FeatureCollection',
-                          'features': [
-                              {
-                                  'type': 'Feature',
-                                  'geometry': {
-                                      'type': 'Point',
-                                      'coordinates': listing.geometry.coordinates
-                                  }
-                              }
-                          ]
-                      }
-                  });
+    //               // Add a data source containing one point feature.
+    //               map.addSource('point', {
+    //                   'type': 'geojson',
+    //                   'data': {
+    //                       'type': 'FeatureCollection',
+    //                       'features': [
+    //                           {
+    //                               'type': 'Feature',
+    //                               'geometry': {
+    //                                   'type': 'Point',
+    //                                   'coordinates': listing.geometry.coordinates
+    //                               }
+    //                           }
+    //                       ]
+    //                   }
+    //               });
   
-                  // Add a layer to use the image to represent the data.
-                  map.addLayer({
-                      'id': 'points',
-                      'type': 'symbol',
-                      'source': 'point', // reference the data source
-                      'layout': {
-                          'icon-image': 'cat', // reference the image
-                          'icon-size': 0.25
-                      }
-                  });
-              }
-          );
-      });
+    //               // Add a layer to use the image to represent the data.
+    //               map.addLayer({
+    //                   'id': 'points',
+    //                   'type': 'symbol',
+    //                   'source': 'point', // reference the data source
+    //                   'layout': {
+    //                       'icon-image': 'cat', // reference the image
+    //                       'icon-size': 0.25
+    //                   }
+    //               });
+    //           }
+    //       );
+    //   });
